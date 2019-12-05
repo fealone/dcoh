e22179def92439cca9faadd8052ed93bc4b3c365
fealone <fealone@lonesec.com>
2019-12-05 21:24:14 +0900

    Support "Transfer-Encoding: chunked" with a custom script by default.

--- lib/content_object.py
+++ lib/content_object.py
@@ -5,7 +5,6 @@ class ContentObject(object):
         self.buf = bytearray()
         self.pos = 0
         self.stream_reader = self.stream()
-        self.previous_chunk = bytearray()
         self.seekable = False
         self.writable = False
         self.finished = False
@@ -27,26 +26,34 @@ class ContentObject(object):
         yield b""
 
     def read(self, size=None):
-        while not self.finished:
+        while 1:
             try:
                 if len(self.buf) < size:
                     self.buf += next(self.stream_reader)
             except StopIteration:
                 if len(self.buf) < size:
-                    self.finished = True
+                    break
             if size:
                 if len(self.buf) >= size:
                     chunk = self.buf[:size]
                     del self.buf[:size]
                     self.pos += size
-                    self.previous_chunk = chunk
-                    return bytes(chunk)
+                    size = hex(len(bytes(chunk))
+                            ).split("0x")[1].upper().encode("utf-8")
+                    return size + b"\r\n" + bytes(chunk) + b"\r\n"
                 continue
-        self.pos += len(self.buf)
-        chunk = self.buf[:]
-        del self.buf[:]
-        self.previous_chunk = chunk
-        return bytes(chunk)
+        if self.buf:
+            self.pos += len(self.buf)
+            chunk = self.buf[:]
+            del self.buf[:]
+            size = hex(len(bytes(chunk))
+                    ).split("0x")[1].upper().encode("utf-8")
+            return size + b"\r\n" + bytes(chunk) + b"\r\n"
+        elif self.finished:
+            return None
+        else:
+            self.finished = True
+            return b"0\r\n\r\n"
 
     def close(self):
         self.finished = True
